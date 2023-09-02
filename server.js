e4d7e577b113f2a6b9880bd8b450211034998c4f
garfustarsius <regroy.tan@gmail.com>
2023-09-02 22:47:41 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const db = require('./db');
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Insert the user into the database
+  db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword], (err) => {
+    if (err) {
+      return res.render('register', { error: 'User already exists' });
+    }
+    res.redirect('/');
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the user exists in the database
+  db.get('SELECT * FROM users WHERE username = ?', [username], async (err, user) => {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.render('login', { error: 'Invalid username or password' });
+    }
+
+    req.session.userId = user.id;
+    res.redirect('/dashboard');
+  });
+});
+
+app.get('/dashboard', (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect('/');
+  }
+  res.send('Welcome to the dashboard!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
